8ea0c5a7bb03743a903be6d65f1e457aedb07a37
kadventure <66458856+kadventure@users.noreply.github.com>
2025-04-29 16:27:22 +0200

    DASH_published_with_option.py
    An optional function, Abort Criterion is added.

--- DASH_published.py
+++ DASH_published.py
@@ -64,7 +64,7 @@ def create_scat(N, P2V, sigma) :
     
     
     figure()
-    imshow(ifftshift(np.angle(scat)), cmap = "hsv"); 
+    imshow(ifftshift(np.angle(scat)), cmap = "hsv", vmin=-pi, vmax=pi); 
     colorbar(); 
     title("scatterer")
     show()
@@ -99,8 +99,8 @@ N = 25 # SLM & scattering media side length in pixels
 N_modes = N**2  #no. of plane wave modes 
 padding = True  #padding in Fourier plane to increase resolution in target plane
 
-P2V = 2*pi  #peak to valley phase of scatterer
-sigma_scat = .1    #gaussian blur kernel sigma of scatterer
+P2V = 1*pi  #peak to valley phase of scatterer
+sigma_scat = 1.0    #gaussian blur kernel sigma of scatterer
 scat = create_scat(N, P2V, sigma_scat) #calculate scatterer
 M = create_modes(N) #calculating stack of plane-wave modes
 
@@ -115,10 +115,20 @@ N_p = 3 # number of phase steps
 eta = 1e3 #2-photon efficieny
 f = 0.2 #energy fraction of the testmodes
 r0 = 1 #initial weight of the first mode
+# r0 = 0.476 # (Optional) Setting Reduction Factor(r0) for the First mode 
 
 sample = make_sample(sample_type) #define fluorescent sample: 2D plane or "bead"
 w = np.zeros((N_i, N_modes), dtype=complex) #init. mode weights
 theta = np.arange(N_p) * 2 * pi / N_p #define phase-stepping angles
+
+# (Optional) Setting Abortion Criterion variables (Optional)
+SlopeDetection = True
+# SlopeDetection = False
+N_test_modes = 24;  # number of examined signals
+slope_percentage = 0.1  # abortion criterion
+latest_sig = np.zeros(N_test_modes)  # abortion criterion
+End_iter_detector = np.zeros([N_i])  # record when the abortion is activated
+break_idx = -1 # counting idx for the abortion
 
 m0 = 1 #start with measuring the second mode in the first DASH iteration (the weight of the first mode is directly calculated from an initial measurement)
 
@@ -175,11 +185,19 @@ for i in range(N_i):
         
         I2ph_stack[mm] = TPEF[0]
         PSF_stack[mm, :, :] = fftshift(PSF)
+
+        # (Optional) Current Iteration Abortion Criterion 
+        if SlopeDetection == True and m >= N_test_modes:
+            latest_sig = I2ph_stack[(mm - N_test_modes+1):(mm+1)]  # lastest series of signals
+            slope = (latest_sig[-1] - latest_sig[0]) / N_test_modes  # measure the slope
+            if m == N_test_modes:  # measure the first slope
+                slope0 = slope
+            elif m > N_test_modes and slope < slope_percentage * slope0:  # compare current slope with the first slope
+                break_idx += 1
+                End_iter_detector[break_idx] = mm
+                break
         mm += 1
-
     C_i[i] = C
-
-
 
 #------showing results------
 
@@ -190,7 +208,7 @@ imshow(PSF_stack[0])
 axis("off")
 colorbar()
 subplot(122)
-imshow(PSF_stack[-1])
+imshow(PSF_stack[mm])
 colorbar()
 axis("off")
 title("Focal int. after corr.")
@@ -210,21 +228,25 @@ if method == "c-DASH":
     title('abs(C)')
     axis('image')
 
-
-
 I2ph_max = measure(1, sample, np.ones((N,N)), padding)[0].item()  #eval. max. signal for the aberration-free case
 
 figure()
-plot(I2ph_stack)
-ylim(0, I2ph_max*1.1)
-xlim(0, N_i * (N**2) - 1)
-xlabel('Mode measurement no.')
-ylabel('2 photon signal / photons')
-axvline(x=N_modes-1, color='r', linestyle='--')
-axvline(x=2*N_modes-1, color='r', linestyle='--')
-axhline(y = I2ph_max, color='g', linestyle='--')
-title(method + ", " + str(N_modes) + " modes , sample = " + sample_type)
+if SlopeDetection == True:  # If Abortion Criterion is used
+    plot(I2ph_stack[:mm])
+    ylim(0, I2ph_max*1.1)
+    for i in range(break_idx+1):    # Draw end of iterations
+        axvline(x=End_iter_detector[i], color='r', linestyle='--', linewidth=1)
+    axhline(y = I2ph_max, color='g', linestyle='--')
+    title(f"Abort Criterion is applied. \n Number of measurements when each iteration ends: \n {End_iter_detector}")
+else:
+    plot(I2ph_stack)
+    ylim(0, I2ph_max*1.1)
+    xlim(0, N_i * (N**2) - 1)
+    xlabel('Mode measurement no.')
+    ylabel('2 photon signal / photons')
+    axvline(x=N_modes-1, color='r', linestyle='--')
+    axvline(x=2*N_modes-1, color='r', linestyle='--')
+    axhline(y = I2ph_max, color='g', linestyle='--')
+    title(method + ", " + str(N_modes) + " modes , sample = " + sample_type)
 show()
 
-
-# %%
